e2d8b6bd0d49507c89e3aaad50485e88bf682b4f
Grant Brown <onlythebest@gmail.com>
2024-07-26 15:28:51 -0700

    configure tests

---
+++ count-substrings/src/test/java/com/grantgotgame/count_substrings/CountSubstringsApplicationTests.java
@@ -0,0 +1,56 @@
+package com.grantgotgame.count_substrings;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountSubstringsApplicationTests {
+	public static CountSubstringsApplication app;
+	@BeforeClass
+	public static void setUp(){
+		app = new CountSubstringsApplication();
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+	// "cat" does not contain "egg", so return 0
+	@Test
+	public void countSubstringsTest1(){
+		String str = "cat";
+		String subStr = "egg";
+		int expected = 0;
+		Assert.assertEquals(expected, app.countSubstrings(str, subStr));
+	}
+
+	// "calendar" contains 1 instance of "lend", so return 1
+	@Test
+	public void countSubstringsTest2(){
+		String str = "calendar";
+		String subStr = "lend";
+		int expected = 1;
+		Assert.assertEquals(expected, app.countSubstrings(str, subStr));
+	}
+
+	// "Mississippi" contains 2 instances of "is", so return 2
+	@Test
+	public void countSubstringsTest3(){
+		String str = "Mississippi";
+		String subStr = "is";
+		int expected = 2;
+		Assert.assertEquals(expected, app.countSubstrings(str, subStr));
+	}
+
+	// "acatOcatecatElCatoNcat" contains 4 instances of "cat", so return 4
+	@Test
+	public void countSubstringsTest4(){
+		String str = "acat12catecatElCatoNcat";
+		String subStr = "cat";
+		int expected = 4;
+		Assert.assertEquals(expected, app.countSubstrings(str, subStr));
+	}
+
+}
